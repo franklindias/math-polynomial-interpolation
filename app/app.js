4cc2310642eac5f4ee2e04def7e3cbff772fcfbd
Franklin Dias <franklindias99@gmail.com>
2015-11-15 15:23:35 -0200

    gerando grafico de acordo com os pontos inseridos

--- app/app.js
+++ app/app.js
@@ -2,11 +2,26 @@ app = angular.module('mathApp', ['chart.js']);
 
 app.controller('LineCtrl', function ($scope) {
 
-  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Series A', 'Series B'];
+	$scope.x1 = 0;
+	$scope.x2 = 0;
+	$scope.y1 = 0;
+	$scope.y2 = 0;
+	$scope.x3 = 0;
+	$scope.y3 = 0;
+
+
+  $scope.labels = [$scope.x1, $scope.x2, $scope.x3];
+  $scope.series = ['Função F(X)'];
   $scope.data = [
-	    [65, 59, 80, 81, 56, 55, 40],
-	    [28, 48, 40, 19, 86, 27, 90]
+	    [$scope.y1, $scope.y2, $scope.y3]
 	  ];
+
+	  $scope.calcular = function(){
+	  	console.log($scope.labels);
+	  	  $scope.labels = [$scope.x1, $scope.x2, $scope.x3];
+		  $scope.data = [
+			    [$scope.y1, $scope.y2, $scope.y3]
+			  ];
+	  }
 });
        
