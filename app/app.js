f1393f67d7da1cc1d236e431be2c837e7ae5cd1e
Franklin Dias <franklindias99@gmail.com>
2015-11-15 14:53:44 -0200

    first commit

---
+++ app/app.js
@@ -0,0 +1,12 @@
+app = angular.module('mathApp', ['chart.js']);
+
+app.controller('LineCtrl', function ($scope) {
+
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [
+	    [65, 59, 80, 81, 56, 55, 40],
+	    [28, 48, 40, 19, 86, 27, 90]
+	  ];
+});
+       
