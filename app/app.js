dd1c0e0726d0f4f9f7ab57cc42707cf36032cf56
Franklin Dias <franklindias@localhost.localdomain>
2015-11-17 14:38:11 -0300

    add input quantpontos e ostrar autocamente os inputs de cada ponto

--- app/app.js
+++ app/app.js
@@ -1,31 +1,44 @@
 app = angular.module('mathApp', ['chart.js']);
 
 app.controller('LineCtrl', function ($scope) {
-	$scope.values = ["Dois Pontos (2)","Três Pontos (3)","Quatro Pontos (4)"];
-    $scope.selectedItem = 0;
 
-	$scope.quantPontos = '2';
-	$scope.x1 = 0;
-	$scope.x2 = 0;
-	$scope.y1 = 0;
-	$scope.y2 = 0;
-	$scope.x3 = 0;
-	$scope.y3 = 0;
-	$scope.x4 = 0;
-	$scope.y4 = 0;
+    $scope.qntPontos = 2;
 
-  $scope.labels = [$scope.x1, $scope.x2, $scope.x3, $scope.x4];
-  $scope.series = ['Função F(X)'];
-  $scope.data = [
-	    [$scope.y1, $scope.y2, $scope.y3, $scope.y4]
+    $scope.x = [];
+    $scope.fx = [];
+
+    $scope.labels = $scope.x;
+    $scope.series = ['Função F(X)'];
+    $scope.data = [
+	    $scope.fx
 	  ];
 
-	  $scope.calcular = function(){
-	  	console.log($scope.labels);
-	  	 $scope.labels = [$scope.x1, $scope.x2, $scope.x3, $scope.x4];
-		  $scope.data = [
-			    [$scope.y1, $scope.y2, $scope.y3, $scope.y4]
+    $scope.calcular = function(){
+	 $scope.gerarGrafico();
+
+    }
+    
+    $scope.gerarGrafico = function(){
+        console.log('funcao gerarGrafico()');
+        console.log('---------------------');
+        console.log();
+        console.log($scope.x);
+        console.log($scope.fx);
+	  	$scope.labels = $scope.x;
+        $scope.data = [
+			    $scope.fx
 			  ];
-	  }
+    }
+    
+    $scope.range = function(){
+        var arratQntPontos = [];
+        var qnt = $scope.qntPontos;
+        
+        for (var i = 0; i < qnt; i++) { 
+            arratQntPontos.push(i) 
+        } 
+        return arratQntPontos;
+    }
+
 });
-       
+
